14310cabb1bad22735cbfabf709e8f50352bc264
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-17 11:58:50 +0100

    10. Loops
    for loops
    while loops
    do loops

---
+++ The Code School/Assets/Intro To S#/Nr10Loops.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nr10Loops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // GÄLLER ALLA!! dom måste bli falska någon gång annars körs koden för alltid och programmet kommer crasha!
+
+
+        for(int i = 0; i < 10; i++) // i++ är samma som att skriva i = 1 + 1
+        {
+            print("The value of i is" + i);
+        }
+
+        int o = 0;
+
+        // så detta e samma som ovan fast ett annat sätt att skriva det.
+        while (o < 10)
+        {
+            print("The value of i is" + o);
+            o++;
+        }
+
+        int y = 0;
+        // do loop utför koden först sen kollar om conditionen stämmer. alla andra kollar först sen kör koden
+        do
+        {
+            print("The value of i is" + y);
+        } while (y < 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
