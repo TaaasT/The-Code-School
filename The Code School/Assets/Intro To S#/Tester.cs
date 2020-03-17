bb97687c499fb43dd897480c4f5f12010837361a
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-17 09:38:16 +0100

    6
    Variables

---
+++ The Code School/Assets/Intro To S#/Tester.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    float power = 2.1f;
+    double health = 3.5; // float o double e typ samma fast float e 32 bit nummer o en double e 64 bit nummer. dvs double e mer precis
+    int score = 44;
+    string PlayerName = "wizard";
+    bool isAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
