bac0ed2874374ef278ac29d09cfe9d7fbe7c75eb
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-17 13:08:25 +0100

    12. arrays o functions

---
+++ The Code School/Assets/Intro To S#/Nr12ArraysOFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nr12ArraysOFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] data = new int[10];
+
+        ProccesArray(data);
+    }
+
+    void ProccesArray(int[] data)
+    {
+        for(int i = 0; i < data.Length; i++)
+        {
+            data[i] = Random.Range(0, 100);
+            print("The value is " + data[i]);
+        }
+    }
+}
