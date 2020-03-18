3c29a1a6a0eb7e8f15390bb77fd98b11d744175d
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-18 11:57:18 +0100

    16. Visibilty Modifiers

---
+++ The Code School/Assets/Intro To S#/Nr16.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nr16
+{
+    private float health = 100f;
+    private string name = "Wizard";
+
+    public Nr16()
+    {
+
+    }
+    public Nr16(string name, float health)
+    {
+        this.name = name;
+        this.health = health;
+    }
+
+    public void PlayerInfo()
+    {
+        Debug.Log("Player Name Is: " + name + "And Players Health Is: " + health);
+    }
+
+
+
+    // samma som nere fast myk smidigare
+    public float Health
+    { get 
+        {
+         return health;
+        }
+      set{
+            health = value;
+        }
+    }    
+
+
+
+    /* Detta e 1:a sätt att göra det på
+    public void SetHealth(float health)
+    {
+        this.health = health;
+    }
+
+    public float GetHealth()
+    {
+        return this.health;
+    }
+
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+    
+    public string GetName()
+    {
+        return this.name;
+    }
+    */
+
+
+}
---
+++ The Code School/Assets/Intro To S#/Nr16VisibilityModifiers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nr16VisibilityModifiers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Nr16 knight = new Nr16("knight", 1);
+
+        knight.Health = 3f;
+
+        knight.PlayerInfo();
+
+
+
+
+
+        /* Detta e 1:a sätt att göra det på 
+        knight.SetHealth(3.4f);
+        print(knight.GetHealth());
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
