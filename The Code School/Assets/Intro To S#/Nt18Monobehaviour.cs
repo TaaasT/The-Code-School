3192eec2a2ea597da02dd158ad668400d51e8bd7
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-19 09:04:35 +0100

    18. Monobehaviour

---
+++ The Code School/Assets/Intro To S#/Nt18Monobehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nt18Monobehaviour : MonoBehaviour
+{
+   //basic grejer kan redan detta..
+    
+    
+    void Start()
+    {
+       
+    }
+
+
+}
