2d571c8f9ddee4d6e402a906f3180a2e614c7437
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-20 13:12:25 +0100

    21. Delegates

---
+++ The Code School/Assets/Intro To S#/Nr21Delegates.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nr21Delegates : MonoBehaviour
+{
+
+    // hela grejen e att detta ska va i en script som inheritar
+    public delegate void PlayerDied();
+    public static event PlayerDied playerDied;
+
+
+    void Start()
+    {
+        if (playerDied != null)
+        {
+            playerDied();
+        }
+    }
+
+    private void OnEnable()
+    {
+        playerDied += ExecutedAfterEventCall;
+    }
+
+    private void OnDisable()
+    {
+        playerDied -= ExecutedAfterEventCall;
+    }
+
+    void ExecutedAfterEventCall()
+    {
+        print("Prited after event was calld");
+    }
+    
+}
