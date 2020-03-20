ddc6d5d952f81d3e9925d57de85e1656d7712425
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-20 12:42:56 +0100

    20. Coroutines

---
+++ The Code School/Assets/Intro To S#/Nr20Coroutines.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nr20Coroutines : MonoBehaviour
+{
+    // måste skapa en metod (IEnumerator) sen kalla på den med StartCoroutine
+    void Start()
+    {
+        StartCoroutine(PrintAfterDelay());
+    }
+
+    IEnumerator PrintAfterDelay()
+    {
+        print("direkt");
+        yield return new WaitForSeconds(2f);
+        print("Hellooo");
+    }
+}
