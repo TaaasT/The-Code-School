dd29c046267d0b9298ea54d97ac7658888ec4824
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-20 13:32:07 +0100

    23. Arrays&Lists

---
+++ The Code School/Assets/Intro To S#/Nr23ArraysOLists.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nr23ArraysOLists : MonoBehaviour
+{
+    //Arrays är fixed nummer.. som nu har vi bara 10 slots.. inte mindre inte mer
+    int[] things = new int[10];
+
+
+    //Lists är smidigare.. kan lägga till hur många jag behöver
+    public List<int> items;
+
+
+    // för att lägga till en plats i en list.
+    void Start()
+    {
+        items.Add(1);
+
+        items.Remove(1);
+
+        for(int i = 0; i < items.Count; i++)
+        {
+            items[i] = 3;
+        }
+    }
+
+    
+}
