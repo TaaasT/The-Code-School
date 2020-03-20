e31c6fdd1a944ed3dd2429f1b5c987ceac923523
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-20 13:19:25 +0100

    22.Enums

---
+++ The Code School/Assets/Intro To S#/Nr22Enumerations.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nr22Enumerations : MonoBehaviour
+{
+
+    //deklarerar variabler
+  public enum GameState
+    {
+        Started,
+        Paused,
+        Ended,
+        MainMenu
+    }
+    // deklarerar först var vi "börjar"
+    GameState gameState = GameState.MainMenu;
+    
+  // hur man ändrar "state"
+    void Start()
+    {
+        if(gameState == GameState.MainMenu)
+        {
+            gameState = GameState.Started;
+        }
+        if(gameState == GameState.Started)
+        {
+            Debug.Log("Our Game has started");
+        }
+    }
+
+    
+}
