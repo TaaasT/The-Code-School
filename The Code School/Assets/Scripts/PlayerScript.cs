1b75492faf324e4aef63759a13175ed648d9831d
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-21 10:33:38 +0100

    24

---
+++ The Code School/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 10f;
+    float minX = -2.55f;
+    float maxX = 2.55f;
+
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector2 currentPos = transform.position;
+
+        if (h > 0)
+        {
+            currentPos.x += speed * Time.deltaTime;
+        }
+        else if(h < 0)
+        {
+            currentPos.x -= speed * Time.deltaTime;
+        }
+
+        if(currentPos.x < minX)
+        {
+            currentPos.x = minX;
+        }
+        if (currentPos.x > maxX)
+        {
+            currentPos.x = maxX;
+        }
+        transform.position = currentPos;
+    }
+}
---
+++ The Code School/Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject bomb;
+
+    float minX = -2.55f;
+    float maxX = 2.55f;
+
+    void Start()
+    {
+        
+    }
+
+    IEnumerator SpawnBombs()
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 1f));
+        Instantiate(bomb, new Vector2(Random.Range(minX, maxX), transform.position.y),
+            Quaternion.identity);
+
+
+    }
+}
